e6492d19717bdd0cadf0e28364041ffc267b5633
Matar Peles <matarpeles@Matars-MacBook-Pro.local>
2023-05-12 17:28:35 +0300

    fix: Used 'get' to replace if statement

--- lambda_function/aws/resources/handler_creator.py
+++ lambda_function/aws/resources/handler_creator.py
@@ -1,14 +1,12 @@
+from typing import Dict, Type
+
+from aws.resources.base_handler import BaseHandler
 from aws.resources.cloudcontrol_handler import CloudControlHandler
 from aws.resources.cloudformation_handler import CloudFormationHandler
-from aws.resources.base_handler import BaseHandler
-from typing import Dict, Type
 
 SPECIAL_AWS_HANDLERS: Dict[str, Type[BaseHandler]] = {"AWS::CloudFormation::Stack": CloudFormationHandler}
 
 
 def create_resource_handler(resource_config, port_client, lambda_context, default_region):
-    if resource_config['kind'] in SPECIAL_AWS_HANDLERS:
-        return SPECIAL_AWS_HANDLERS[resource_config['kind']](resource_config, port_client, lambda_context,
-                                                             default_region)
-
-    return CloudControlHandler(resource_config, port_client, lambda_context, default_region)
+    handler = SPECIAL_AWS_HANDLERS.get(resource_config['kind'], CloudControlHandler)
+    return handler(resource_config, port_client, lambda_context, default_region)
