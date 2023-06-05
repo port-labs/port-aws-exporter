5f941d44a1cfccf2cbe86b2ca526239596c1ffca
Tal Fishler <tal.fishler@gong.io>
2023-06-05 11:07:00 +0300

    (feat) - New class for handling external resource handlers
    * new extended_resource_handler.py class to manage all common logic.
    * ASG and SG handlers implementing an example of usage.

--- lambda_function/aws/resources/handler_creator.py
+++ lambda_function/aws/resources/handler_creator.py
@@ -3,9 +3,14 @@ from typing import Dict, Type
 from aws.resources.base_handler import BaseHandler
 from aws.resources.cloudcontrol_handler import CloudControlHandler
 from aws.resources.cloudformation_handler import CloudFormationHandler
+from aws.resources.security_group_handler import SecurityGroupHandler
+from aws.resources.asg_handler import ASGHandler
 
-SPECIAL_AWS_HANDLERS: Dict[str, Type[BaseHandler]] = {"AWS::CloudFormation::Stack": CloudFormationHandler}
-
+SPECIAL_AWS_HANDLERS: Dict[str, Type[BaseHandler]] = {
+    "AWS::CloudFormation::Stack": CloudFormationHandler,
+    'AWS::EC2::SecurityGroup': SecurityGroupHandler,
+    'AWS::AutoScaling::AutoScalingGroup': ASGHandler,
+}
 
 def create_resource_handler(resource_config, port_client, lambda_context, default_region):
     handler = SPECIAL_AWS_HANDLERS.get(resource_config['kind'], CloudControlHandler)
