9af43c3c92607277ba1529db16720496d8fa45a6
PeyGis <isaac.p.coffie@gmail.com>
2023-06-22 12:48:04 +0000

    resolve PR comments

--- lambda_function/aws/resources/elastic_cloud_compute_handler.py
+++ lambda_function/aws/resources/elastic_cloud_compute_handler.py
@@ -1,18 +1,16 @@
 import json
 import logging
-from collections import OrderedDict
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import boto3
 import consts
-import yaml
 from aws.resources.base_handler import BaseHandler
 from port.entities import create_entities_json, handle_entities
 
 logger = logging.getLogger(__name__)
 
 
-class EC2Handler(BaseHandler):
+class EC2InstanceHandler(BaseHandler):
     def handle(self):
         for region in list(self.regions):
             aws_ec2_client = boto3.resource('ec2', region_name=region)
@@ -23,10 +21,7 @@ class EC2Handler(BaseHandler):
                 if self.next_token:
                     list_instances_params['NextToken'] = self.next_token
                 try:
-                    response = aws_ec2_client.instances.all()
-                    instance_list = list()
-                    for instance in response:
-                        instance_list.append({"id": instance.id})
+                    instance_list = aws_ec2_client.instances.all()
 
                 except Exception as e:
                     logger.error(
@@ -37,8 +32,8 @@ class EC2Handler(BaseHandler):
                     break
 
                 self._handle_list_response(instance_list, region)
+                self.next_token = None ## Since the instances.all() returns all instances, there is no need to do pagination
 
-                #self.next_token = instance_list.get('NextToken')
                 if self.lambda_context.get_remaining_time_in_millis() < consts.REMAINING_TIME_TO_REINVOKE_THRESHOLD:
                     # Lambda timeout is too close, should return checkpoint for next run
                     return self._handle_close_to_timeout(region)
@@ -49,7 +44,7 @@ class EC2Handler(BaseHandler):
 
     def _handle_list_response(self, list_response, region):
         with ThreadPoolExecutor(max_workers=consts.MAX_UPSERT_WORKERS) as executor:
-            futures = [executor.submit(self.handle_single_resource_item, region, instance.get("id")) for instance in list_response]
+            futures = [executor.submit(self.handle_single_resource_item, region, instance.id) for instance in list_response]
             for completed_future in as_completed(futures):
                 result = completed_future.result()
                 self.aws_entities.update(result.get('aws_entities', set()))
@@ -66,13 +61,13 @@ class EC2Handler(BaseHandler):
                 aws_ec2_client = boto3.client("ec2", region_name=region)
                 instance_response = aws_ec2_client.describe_instances(InstanceIds=[instance_id])
                 instance_obj = instance_response["Reservations"][0]["Instances"][0]
+                instance_obj["Region"] = region ## The instance object doesn't have region so we add it dynamically here
 
                 # Handles unserializable date properties in the JSON by turning them into a string
                 instance_obj = json.loads(json.dumps(instance_obj, default=str))
 
             elif action_type == 'delete':
                 instance_obj = {"identifier": instance_id}  # Entity identifier to delete
-
             entities = create_entities_json(instance_obj, self.selector_query, self.mappings, action_type)
 
         except Exception as e:
