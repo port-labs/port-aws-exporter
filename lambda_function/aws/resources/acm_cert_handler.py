e49b9e72e963cc4ef8d6a55c1e314ef5ec652795
Charles Dunda <s33dunda@gmail.com>
2023-10-25 16:41:00 -0400

    fixup: update acm handler with pr review requests
    - Use the NextToken Pattern
    - allow list_parameters to allow other filters
    - add log for list with current region
    - add next_token self._cleanup_regions

--- lambda_function/aws/resources/acm_cert_handler.py
+++ lambda_function/aws/resources/acm_cert_handler.py
@@ -9,51 +9,45 @@ from port.entities import create_entities_json, handle_entities
 
 logger = logging.getLogger(__name__)
 
-
 class ACMHandler(BaseHandler):
     def handle(self):
         for region in list(self.regions):
             aws_acm_client = boto3.client("acm", region_name=region)
-            try:
-                paginator = aws_acm_client.get_paginator('list_certificates')
-                for page in paginator.paginate():
-                    certificate_summary_list = page.get('CertificateSummaryList', [])
-                    self._handle_list_response(certificate_summary_list, region)
+            logger.info(f"List ACM Certificates, region: {region}")
+            self.next_token = "" if self.next_token is None else self.next_token
+            while self.next_token is not None:
+                list_certificates_params = self.selector_aws.get("list_parameters", {})
+                if self.next_token:
+                    list_certificates_params["NextToken"] = self.next_token
+                try:
+                    response = aws_acm_client.list_certificates(**list_certificates_params)
+                except Exception as e:
+                    logger.error(
+                        f"Failed to list ACM Certificates, region: {region},"
+                        f" Parameters: {list_certificates_params}; {e}")
+                    self.skip_delete = True
+                    self.next_token = None
+                    break
 
-                    if self.lambda_context.get_remaining_time_in_millis() < consts.REMAINING_TIME_TO_REINVOKE_THRESHOLD:
-                        # Lambda timeout is too close, should return checkpoint for next run
-                        return self._handle_close_to_timeout(region)
-            except Exception as e:
-                logger.error(f"Failed to list ACM certificates in region: {region}; {e}")
-                self.skip_delete = True
+                self._handle_list_response(response, region)
 
-        return {
-            "aws_entities": self.aws_entities,
-            "next_resource_config": None,
-            "skip_delete": self.skip_delete,
-        }
+                self.next_token = response.get("NextToken")
+                if self.lambda_context.get_remaining_time_in_millis() < consts.REMAINING_TIME_TO_REINVOKE_THRESHOLD:
+                    # Lambda timeout is too close, should return checkpoint for next run
+                    return self._handle_close_to_timeout(region)
 
-    def _handle_list_response(self, certificate_summary_list, region):
-        with ThreadPoolExecutor(max_workers=consts.MAX_CC_WORKERS) as executor:
-            futures = [
-                executor.submit(self.handle_single_resource_item, region, cert.get("CertificateArn", ""))
-                for cert in certificate_summary_list
-            ]
+            self._cleanup_regions(region)
+
+        return {"aws_entities": self.aws_entities, "next_resource_config": None, "skip_delete": self.skip_delete}
+
+    def _handle_list_response(self, list_response, region):
+        certificates = list_response.get("CertificateSummaryList", [])
+        with ThreadPoolExecutor(max_workers=consts.MAX_DEFAULT_AWS_WORKERS) as executor:
+            futures = [executor.submit(self.handle_single_resource_item, region, cert.get("CertificateArn")) for cert in certificates]
             for completed_future in as_completed(futures):
                 result = completed_future.result()
                 self.aws_entities.update(result.get("aws_entities", set()))
                 self.skip_delete = result.get("skip_delete", False) if not self.skip_delete else self.skip_delete
-
-    def _handle_close_to_timeout(self, region):
-        if "selector" not in self.resource_config:
-            self.resource_config["selector"] = {}
-        self.resource_config["selector"]["aws"] = self.selector_aws
-
-        return {
-            "aws_entities": self.aws_entities,
-            "next_resource_config": self.resource_config,
-            "skip_delete": self.skip_delete,
-        }
 
     def handle_single_resource_item(self, region, certificate_arn, action_type="upsert"):
         entities = []
@@ -65,7 +59,7 @@ class ACMHandler(BaseHandler):
                 aws_acm_client = boto3.client("acm", region_name=region)
                 response = aws_acm_client.describe_certificate(CertificateArn=certificate_arn)
                 resource_obj = response.get("Certificate", {})
-
+                
                 # Handles unserializable date properties in the JSON by turning them into a string
                 resource_obj = json.loads(json.dumps(resource_obj, default=str))
             elif action_type == "delete":
@@ -78,3 +72,18 @@ class ACMHandler(BaseHandler):
         aws_entities = handle_entities(entities, self.port_client, action_type)
 
         return {"aws_entities": aws_entities, "skip_delete": skip_delete}
+
+    def _handle_close_to_timeout(self, region):
+        if self.next_token:
+            self.selector_aws["next_token"] = self.next_token
+        else:
+            self.selector_aws.pop("next_token", None)
+            self._cleanup_regions(region)
+            if not self.regions:  # Nothing left to sync
+                return {"aws_entities": self.aws_entities, "next_resource_config": None, "skip_delete": self.skip_delete}
+
+        if "selector" not in self.resource_config:
+            self.resource_config["selector"] = {}
+        self.resource_config["selector"]["aws"] = self.selector_aws
+
+        return {"aws_entities": self.aws_entities, "next_resource_config": self.resource_config, "skip_delete": self.skip_delete}
